dc59382cbeb091c0e6b94c78aff6df38f8834b4b
Sujan299 <mrsujan321@gmail.com>
2025-01-03 11:26:35 +0545

    modified post.controller.js

--- server/controllers/posts.controller.js
+++ server/controllers/posts.controller.js
@@ -2,40 +2,43 @@ import BlogPost from '../models/blogPost.model.js';
 
 // create post
 
-export const create_post = async(req, res)=>{
-    try{
-        const {title,image, content, author, tags, category} = req.body;
-        const post = new BlogPost({title,image, content, author, tags, category});
+export const create_post = async (req, res) => {
+    try {
+        const { title, image, content, author, tags, category } = req.body;
+        const post = new BlogPost({ title, image, content, author, tags, category });
         await post.save();
         res.status(201).send("added successfully !");
     }
-    catch(err){
+    catch (err) {
         console.log(err);
     }
 }
 
 // get all post 
 
-export const get_all_posts = async (req, res)=>{
-    try{
+export const get_all_posts = async (req, res) => {
+    try {
         const users = await BlogPost.find();
-        res.status(200).json(users);
-    }catch(err){
+        if (users.length === 0) {
+            return res.status(200).json({ message: "No posts found" });
+        }
+        res.status(200).json(posts);
+    } catch (err) {
         console.log("Can not get all users", err);
     }
 }
 
 // get a post
 
-export const get_a_post = async (req, res)=>{
-    try{
+export const get_a_post = async (req, res) => {
+    try {
         const post = await BlogPost.findById(req.params.id);
-        if(!post){
+        if (!post) {
             return res.status(404).send("post not found!");
         }
         res.status(200).send(post);
     }
-    catch(err){
-        res.status(500).json({message: "server error"});
+    catch (err) {
+        res.status(500).json({ message: "server error" });
     }
 }
